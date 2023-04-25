9ef36d74a49a69b43f7cff8a47ce783f05edb184
imarquezz <i.marquez.2020@alumnos.urjc.es>
2023-04-25 18:39:23 +0200

    feat: decision tree implementation

---
+++ src/detecciónUsuarios/decisionTree.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+from sklearn import tree
+import json
+
+def checkPrediction(predict, data):
+    aciertos= 0
+    fallos= 0
+    for i in range(0,len(predict)):
+        if predict[i]==data[i]: aciertos+=1
+        else: fallos +=1
+    return aciertos,fallos
+
+f = open('../../devices_IA_clases.json')
+data_train = json.load(f)
+f.close()
+f = open('../../devices_IA_predecir.json')
+data_predict = json.load(f)
+f.close()
+
+data_x_train = []
+data_y_train = []
+data_x_predict = []
+
+for user in data_train:
+    data_x_train.append([user['servicios'],user['servicios_inseguros']])
+    data_y_train.append(user['peligroso'])
+
+for user in data_predict:
+    data_x_predict.append([user['servicios'],user['servicios_inseguros']])
+    #data_y_predict.append(user['peligroso'])
+
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(data_x_train, data_y_train)
+
+data_y_predict = clf.predict(data_x_predict)
+data_y = data_y_train
+aciertos,fallos = checkPrediction(data_y_predict,data_y)
+print("ACIERTOS => " + str(aciertos))
+print("FALLOS => " + str(fallos))
+
+
+tree.plot_tree(clf,filled=True,feature_names=["servicios","servicios_inseguros"],fontsize=7,class_names=["no peligroso","peligroso"])
+plt.show()
